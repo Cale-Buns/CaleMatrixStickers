3c81509f6d606d6cd32ffab764387c9b31d45350
Cale-Buns <calebunns@gmail.com>
2022-12-18 14:50:39 -0500

    initial commit
    stickers initial commit

---
+++ sticker/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'fe11a104'
+version = '0.1.0+dev.fe11a104'
+linkified_version = '0.1.0+dev.[fe11a104](https://github.com/maunium/stickerpicker/commit/fe11a1040c0cc5cfe8818e69f864fb71a07b359c)'
